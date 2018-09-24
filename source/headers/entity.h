b68e3a19f77dab755b7a5f10a6ae813273159144
Jamie Billings <jamiebillings@sky.com>
2018-09-24 14:16:40 +0100

    testing structs insted of classes

--- source/headers/entity.h
+++ source/headers/entity.h
@@ -1,7 +1,57 @@
 #ifndef ENTITY_H_INCLUDED
 #define ENTITY_H_INCLUDED
-
+//CLEAN UP
 //Change to Struct System with void* entity List
+
+enum EntityState
+{
+	is_grounded = 1,
+	is_flipped 	= 2,
+	is_still 	= 4,
+	is_walking 	= 8,
+	is_jumping 	= 16
+}
+
+enum Identifer
+{
+	
+}
+
+struct BaseEntity
+{
+	static Texture sprite;
+
+	float pos_x;
+	float pos_y;
+	int width;
+	int height;
+
+	Vector2D velocity;
+	Vector2D max_velocity;
+
+	float jump_height;
+
+	EntityState state;
+};
+
+struct Player
+{
+	
+};
+
+namespace Entity
+{
+	//Static Functions
+	void Init();
+	void Deinit();
+
+	void Create(BaseEntity _self, SDL_Rect _base_pos);
+	void Create(Player _self, SDL_Rect _base_pos);
+	void Update();
+	void Render();
+	void Destroy();
+}
+
 class BaseEntity
 {
 public:
