b094fe3230660989a9a34d06dafe8ad0c1b67941
Jamie Billings <jamiebillings@sky.com>
2019-02-22 16:52:57 +0000

    Working more on the Texture Atlas Tool

--- Tools/Texture Atlas/main.cpp
+++ Tools/Texture Atlas/main.cpp
@@ -3,13 +3,17 @@
 #include <string>
 
 #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image.h"
+#include "stb_image_write.h"
 
 /*  Mask
         Single Composistion
         Black = 0
         White = 255
 */
+
+//@@ TO DO make it so I can get a 32bit pixel array so I dont have to do maths when trying to copy pixel data over.
 
 class Image
 {
@@ -24,14 +28,21 @@ public:
 
     void LoadImage(std::string _source);
     void LoadMask(std::string _source);
+    void Free();
 };
 
 class Box
 {
+public:
+
     int x;
     int y;
     int width;
     int height;
+
+    void Create(int _x, int _y, int _width, int _height);
+    bool Compare(Box _box);
+    bool Compare(int _x, int _y);
 };
 
 /*  ARGV
@@ -41,6 +52,9 @@ class Box
         2 = Edited image location
         3 = Atlas text file location
 */
+
+int mask_array_count = 0;
+Box mask_array[100];
 
 int main(int argc, char** argv)
 {
@@ -53,11 +67,42 @@ int main(int argc, char** argv)
             return -1; 
     }
 
-    Image new_image;
-    new_image.LoadImage(argv[1]);
-
     Image mask_image;
     mask_image.LoadMask(argv[2]);
+
+    Image old_image;
+    old_image.LoadImage(argv[1]);
+
+    for(int i = 0; i < mask_array_count; i++){
+        printf("Box #%d | x: %d | y: %d | w: %d | h: %d \n", i, mask_array[i].x, mask_array[i].y, mask_array[i].width, mask_array[i].height);
+    }
+
+    //@@ Change later so the the new image changes in doubles 2x2 4x4 8x8 16x16 32x32
+    Image new_image;
+    new_image.width = old_image.width;
+    new_image.height = old_image.height;
+    new_image.pixel_data = static_cast<unsigned char*>(malloc(old_image.width * old_image.height * 4));
+
+    int start_pos_x = 0;
+    int start_pos_y = 0;
+
+    //@@ Add Y scanning to copy whole image
+    for(int i = 0; i < mask_array_count; i++){
+        for(int x = 0; x < (mask_array[i].width * 4); x+=4){
+            new_image.pixel_data[start_pos_x + x] = old_image.pixel_data[20 * old_image.width + mask_array[i].x + x];
+            new_image.pixel_data[start_pos_x + x + 1] = old_image.pixel_data[20 * old_image.width + mask_array[i].x + x + 1];
+            new_image.pixel_data[start_pos_x + x + 2] = old_image.pixel_data[20 * old_image.width + mask_array[i].x + x + 2];
+            new_image.pixel_data[start_pos_x + x + 3] = old_image.pixel_data[20 * old_image.width + mask_array[i].x + x + 3];
+        }
+
+        start_pos_x += mask_array[i].width * 4;
+    }
+
+    stbi_write_png("Testings.png", new_image.width, new_image.height, 4, new_image.pixel_data, new_image.width * 4);
+
+    free(new_image.pixel_data);
+    old_image.Free();
+    mask_image.Free();
 
 
     return 0;
@@ -68,8 +113,6 @@ void Image::LoadImage(std::string _source)
     pixel_data = stbi_load(_source.c_str(), &width, &height, &componants, 0);
 
     printf("| Image Width %d | Image Height %d |\n", width, height);
-
-    stbi_image_free(pixel_data);
 }
 
 void Image::LoadMask(std::string _source)
@@ -85,16 +128,65 @@ void Image::LoadMask(std::string _source)
 
     for(int y = 0; y < height; y++){
     for(int x = 0; x < width; x++){
-        if(pixel_data[y * width + x] < 255){
-            //Scan for the bounds of the mask
-            for(int i = 0; i < width - x; i++){
-                if(pixel_data[y * width ]) //stuff about the width - x here
+        if(pixel_data[y * width + x] != 255){
+            bool matching = false;
+            for(int i = 0; i < mask_array_count; i++){
+                if(mask_array[i].Compare(x,y)){matching = true;}
             }
+            if(matching){continue;}
+
+            int scan_x = 0;
+            for(scan_x = x; scan_x < width; scan_x++){
+                if(pixel_data[y * width + scan_x] == 255){break;}
+            }
+
+            int scan_y = 0;
+            for(scan_y = y; scan_y < height; scan_y++){
+                if(pixel_data[scan_y * width + x] == 255){break;}
+            }
+
+            Box new_box;
+            new_box.Create(x, y, scan_x - x, scan_y - y);
+
+            mask_array[mask_array_count] = new_box;
+            mask_array_count++;
+
         }
     }
     }
-    
-    printf("COMPO %d \n", componants);
+}
 
+void Image::Free()
+{
     stbi_image_free(pixel_data);
+}
+
+void Box::Create(int _x, int _y, int _width, int _height)
+{
+    x = _x;
+    y = _y;
+    width  = _width;
+    height = _height;
+}
+
+bool Box::Compare(Box _box)
+{
+    if(_box.x >= x && _box.x <= x + width){
+        if(_box.y >= y && _box.y <= y + height){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool Box::Compare(int _x, int _y)
+{
+    if(_x >= x && _x <= x + width){
+        if(_y >= y && _y <= y + height){
+            return true;
+        }
+    }
+
+    return false;
 }
