2401fb75adbadd0c0853d0fdfece4a2f8ae3fe6e
Jamie Billings <jamiebillings@sky.com>
2019-03-13 16:57:37 +0000

    Working on tool

--- Tools/Texture Atlas/main.cpp
+++ Tools/Texture Atlas/main.cpp
@@ -68,7 +68,7 @@ int main(int argc, char** argv)
     }
 
     Image mask_image;
-    //mask_image.LoadMask(argv[2]);
+    mask_image.LoadMask(argv[2]);
 
     Image old_image;
     old_image.LoadImage(argv[1]);
@@ -76,59 +76,72 @@ int main(int argc, char** argv)
     for(int i = 0; i < mask_array_count; i++){
         printf("Box #%d | x: %d | y: %d | w: %d | h: %d \n", i, mask_array[i].x, mask_array[i].y, mask_array[i].width, mask_array[i].height);
     }
+    printf("BOX COUNT: %d\n", mask_array_count);
         //@@ Everthing below here doesnt work
+
+    //@@add a endian check : Project will only build for Little Endian
+
+
 
     //@@ Change later so the the new image changes in doubles 2x2 4x4 8x8 16x16 32x32
     Image new_image;
     new_image.width = old_image.width;
     new_image.height = old_image.height;
-    new_image.pixel_data = static_cast<uint8_t*>(malloc(old_image.width * old_image.height * 4)); //@@Maybe not needed
+    //new_image.pixel_data =  //@@Maybe not needed
 
     uint32_t* old_pixel_data = reinterpret_cast<uint32_t*>(old_image.pixel_data);
-    uint8_t* new_pixel_data = new_image.pixel_data; //reinterpret_cast<uint32_t*>(new_image.pixel_data);
+    uint32_t* new_pixel_data = static_cast<uint32_t*>(malloc(old_image.width * old_image.height)); //reinterpret_cast<uint32_t*>(new_image.pixel_data);
 
-    printf("OLD1: %d | NEW1: %d \n", old_image.pixel_data[0] ,old_pixel_data[0]);
-    printf("OLD2: %d | NEW2: %d \n", old_image.pixel_data[1] ,old_pixel_data[1]);
-    printf("OLD3: %d | NEW3: %d \n", old_image.pixel_data[2] ,old_pixel_data[2]);
-    printf("OLD4: %d | NEW4: %d \n", old_image.pixel_data[3] ,old_pixel_data[3]);
-
+    //Testing the pixel data @@remove later
+    //for(int i = 0; i < 5*5; i++){new_pixel_data[i] = old_pixel_data[i];}
+    if(memset(new_pixel_data, 255, old_image.width * old_image.height * 4) == nullptr){
+        printf("MEMORY ERROR\n");
+    } // Alpha = 255 ; rest = 0
 
     int start_pos_x = 0;
     int start_pos_y = 0;
 
     //@@ Testing the output
     //@@ Add Y scanning to copy whole image
-    for(int y = 0; y < old_image.height ; y++){
-        for(int x = 0; x < old_image.width * 4; x++)
-        {
-            //Testing the output
-            new_image.pixel_data[y * old_image.width + x] = old_image.pixel_data[y * old_image.height + x];
-            //printf("OLD : %d | NEW: %d \n", old_image.pixel_data[y * old_image.height + x] ,new_image.pixel_data[y * old_image.width + x]);
-        }
-    }
-
-    for(int i = 0; i < old_image.height * old_image.width; i++){
-        //if(old_pixel_data[i] != new_pixel_data[i]){printf("Error on: %d\n", i);}
-    }
-    
-    /*SS
     for(int i = 0; i < mask_array_count; i++){
-        for(int x = 0; x < (mask_array[i].width * 4); x+=4){
-            new_image.pixel_data[start_pos_x + x] = old_image.pixel_data[20 * old_image.width + mask_array[i].x + x];
-            new_image.pixel_data[start_pos_x + x + 1] = old_image.pixel_data[20 * old_image.width + mask_array[i].x + x + 1];
-            new_image.pixel_data[start_pos_x + x + 2] = old_image.pixel_data[20 * old_image.width + mask_array[i].x + x + 2];
-            new_image.pixel_data[start_pos_x + x + 3] = old_image.pixel_data[20 * old_image.width + mask_array[i].x + x + 3];
+        //box[i];
+        printf("Testing Box :%i\n",i);
+
+        for(int y = start_pos_y; y < start_pos_y + mask_array[i].height; y++){
+        for(int x = start_pos_x; x < start_pos_x + mask_array[i].width; x++){
+
+        /* 
+           new_pixel_data[y * old_image.width + x] = old_pixel_data[(mask_array[i].y + (y - start_pos_y)) * old_image.width 
+                + (mask_array[i].x + (x - start_pos_x))];
+                */
+        }
         }
 
-        start_pos_x += mask_array[i].width * 4;
-    }*/
+        //Offset the start pos so the next box doesnt overwrite the image
+        if(start_pos_x + mask_array[i].width > old_image.width){
+            start_pos_y += mask_array[i].height;
+            start_pos_x = 0;
+        }
+        else{
+            start_pos_x += mask_array[i].width;
+        }
+        
+    }
 
-    uint8_t* return_pixel_data = reinterpret_cast<uint8_t*>(new_pixel_data);
+    uint8_t* return_cast = reinterpret_cast<uint8_t*>(new_pixel_data);
+
+    for(int i = 0; i < 25; i++)
+    {
+        printf("%x : %x \n",old_pixel_data[i],new_pixel_data[i]);
+    }
 
 
+    printf("FIXED BOXES\n");
+    //CRASHES HERE PROBABLY DUE TO BAD MEMORY
+    stbi_write_png("Testings.png", old_image.width, old_image.height, 4, return_cast, old_image.width * 4);
 
-    stbi_write_png("Testings.png", old_image.width, old_image.height, 4, old_pixel_data, old_image.width * 4);
 
+    printf("FIXED DRAWING\n");
     free(new_image.pixel_data);
     old_image.Free();
     mask_image.Free();
@@ -144,7 +157,7 @@ void Image::LoadImage(std::string _source)
 
     stbi_write_png("Testing2.png", width, height, 4, pixel_data, width * componants);
 
-    printf("| Image Width %d | Image Height %d | BYTE Data Count: %d \n", width, height, width * height * componants);
+    //printf("| Image Width %d | Image Height %d | BYTE Data Count: %d \n", width, height, width * height * componants);
 }
 
 void Image::LoadMask(std::string _source)
